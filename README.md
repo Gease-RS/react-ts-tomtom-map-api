132cbc4dba06b3f24772f7aafa010eb2d357d6c9
Gease-RS <geasebrasil@gmail.com>
2022-04-22 21:56:12 -0300

    Conectando projeto a API Tomtom

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
+<div align="center">
+  <p align="center">
+    <img src="src/assets/img/tomtom-api-key.png" width="680" height="290" alt="Tomtom Api Maps" />
+  </p>
+ <h1>Tomtom Api Maps</h1>
+ <h3>Alternativa para subistituir Google Maps<h3>
+</div>
 
-### Iniciando Projeto
+1. Crie o projeto React Typescript
 
-```
+```bash
 $ npx create-react-app tomtom-map-api --template=typescript
 ```
+
+2. Crie a chave api em: https://developer.tomtom.com/
+
+3. Instale o pacote Tomtom
+
+```bash
+$ npm i @tomtom-international/web-sdk-maps
+```
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import tt from '@tomtom-international/web-sdk-maps';
 
-function App() {
+
+const App: React.FC = () => {
+
+  const [map, setMap] = useState({});
+
+  useEffect(( ) => {
+    let map = tt.map({
+      "key": `${process.env.REACT_APP_TOMTOM_API_KEY}`,
+      "container": 'map',
+    })
+    console.log('map', map)
+
+    setMap(map)
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="map"></div>
+    </>
   );
 }
 
 export default App;
+
+
+
