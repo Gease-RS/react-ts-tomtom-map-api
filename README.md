469c46828de860e15ee6e05288b5f1dbcaf8531c
Gease-RS <geasebrasil@gmail.com>
2022-04-24 23:27:19 -0300

    Resolver errors

--- README.md
+++ README.md
@@ -7,15 +7,22 @@
 </div>
 
 1. Crie o projeto React Typescript
-
 ```bash
 $ npx create-react-app tomtom-map-api --template=typescript
 ```
 
 2. Crie a chave api em: https://developer.tomtom.com/
 
-3. Instale o pacote Tomtom
-
+3. Instale o pacote Tomtom Maps
 ```bash
 $ npm i @tomtom-international/web-sdk-maps
+```
+4. Instale Tomtom Services
+```
+$ npm i @tomtom-international/web-sdk-services
+```
+
+5. Instale React-toastify
+```
+$ npm i react-toastify
 ```
--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,46 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
-import tt from "@tomtom-international/web-sdk-maps";
+
+import * as tt from '@tomtom-international/web-sdk-maps'
+import * as ttapi from '@tomtom-international/web-sdk-services'
 import "@tomtom-international/web-sdk-maps/dist/maps.css";
 
-const App: React.FC = (y) => {
+
+const App: React.FC = () => {
+  const mapElement = useRef<HTMLDivElement>({} as HTMLDivElement);
   const [map, setMap] = useState({});
   const [latitude, setLatitude] = useState(-29.166775713057103);
   const [longitude, setLongitude] = useState(-51.51681979959098);
+
+  const convertPoints = (lngLat: tt.LngLat) => {
+    return { 
+      point: {
+        longitude: lngLat.lng,
+        latitude: lngLat.lat,
+      }
+     };
+  }
+
+  const addDeliveryMarker = (lngLat: tt.LngLat, map: tt.Map) => {
+    const element = document.createElement('div')
+    element.className = 'marker-delivery'
+    new tt.Marker({
+      element: element
+    })
+    .setLngLat(lngLat)
+    .addTo(map)
+  }
   
-  function setDataLatitude(e: any) {
-    e.preventDefault();
-    setLatitude(e.target.elements.latitude.value);
-  }
-
-  function setDataLongitude(e: any) {
-    e.preventDefault();
-    setLongitude(e.target.elements.longitude.value);
-  }
-
   useEffect(() => {
+    const origin = {
+      lng: longitude,
+      lat: latitude,
+    }
+    const destinations = []
+
     let map = tt.map({
       key: `${process.env.REACT_APP_API_KEY_TOMTOM}`,
-      container: "map",
+      container: mapElement.current,
       stylesVisibility: {
         trafficIncidents: true,
         trafficFlow: true,
@@ -41,40 +60,49 @@ const App: React.FC = (y) => {
       .setLngLat([longitude, latitude])
       .addTo(map);
 
-      marker.on("dragend", (e) => {
+      marker.on("dragend", () => {
         const lngLat = marker.getLngLat();
         setLongitude(lngLat.lng);
         setLatitude(lngLat.lat);
       })
     }
+    addMarker()
 
+    const pointsForDestinations = (locations: any[]) => {
+      const pointsForDestinations = locations.map((destination) => {
+        return convertPoints(destination)
+      })
 
+    const callParameters = {
+      key: `${process.env.REACT_APP_API_KEY_TOMTOM}`,
+      destinations: pointsForDestinations,
+      origins: [convertPoints(origin)]
+    }
 
-    addMarker();
+    return new Promise((resolve, reject) => {
+      ttapi.services.matrixRouting(callParameters)
+    })
+
+    map.on('click', (e) => {
+      destinations.push(e.lngLat)
+      addDeliveryMarker(e.lngLat, map)
+    })
 
     return () => map.remove();
   }, [longitude, latitude]);
 
   return (
     <>
-      {map && (
-        <div className="app">
-          <div id="map" />
-          <div className="search-bar"></div>
-          <h1>Pesquise</h1>
-          <input
-            type="text"
-            placeholder="Digite a longitude"
-            onChange={setDataLatitude}
-          />
-          <input
-            type="text"
-            placeholder="Digite a latitude"
-            onChange={setDataLongitude}
-          />
-          <button>Pesquisar</button>
+        <div id="map" ref={mapElement}>
+          <div className="app"> 
+          </div>
         </div>
-      )}
+        <div className="search-bar">
+            <label>Longitude:</label>
+            <input type="text" name="longitude" value={longitude} onChange={(e) => setLongitude(parseFloat(e.target.value))} />
+            <label>Latitude:</label>
+            <input type="text" name="latitude" value={latitude} onChange={(e) => setLatitude(parseFloat(e.target.value)) } />
+        </div>
     </>
   );
 };
